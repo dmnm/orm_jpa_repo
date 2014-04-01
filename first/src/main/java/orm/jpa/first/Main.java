ea66e6d527e0b36e362a570360791ff27021e3ae
dmnm <dm-nm@nm.ru>
2014-04-01 09:16:24 +0400

    2

--- first/src/main/java/orm/jpa/first/Main.java
+++ first/src/main/java/orm/jpa/first/Main.java
@@ -1,14 +1,33 @@
 package orm.jpa.first;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.persistence.Query;
+
+import orm.jpa.first.entity.FirstEntity;
 
 public class Main {
 
 	public static void main(String[] args) {
-		EntityManagerFactory emf = Persistence.createEntityManagerFactory(null);
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("default");
 		EntityManager em = emf.createEntityManager();
+
+		em.getTransaction().begin();
+
+		Query query = em.createQuery("SELECT e FROM FirstEntity e WHERE e.name = 'First'");
+		List<FirstEntity> results = query.getResultList();
+
+
+		for (FirstEntity entity : results) {
+			entity.setAmount(entity.getAmount() * 2);
+		}
+
+		em.getTransaction().commit();
+
+		em.close();
 	}
 
 }
--- first/src/main/java/orm/jpa/first/entity/FirstEntity.java
+++ first/src/main/java/orm/jpa/first/entity/FirstEntity.java
@@ -1,8 +1,38 @@
 package orm.jpa.first.entity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 public class FirstEntity {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+	@Column
+	private String name;
+	@Column
+	private Long amount;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Long getAmount() {
+		return amount;
+	}
+	public void setAmount(Long amount) {
+		this.amount = amount;
+	}
 
 }
