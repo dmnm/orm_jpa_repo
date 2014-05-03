d71db5abff598d7d8e1293226385efc7d8d3478b
dmnm <dm-nm@nm.ru>
2014-05-03 22:17:27 +0400

    10.3

--- inheritance/src/main/java/orm/jpa/entity/Project_.java
+++ inheritance/src/main/java/orm/jpa/entity/Project_.java
@@ -5,10 +5,10 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="Dali", date="2014-05-03T20:38:01.709+0400")
+@Generated(value="Dali", date="2014-05-03T21:30:45.993+0400")
 @StaticMetamodel(Project.class)
 public class Project_ {
+	public static volatile SingularAttribute<Project, Long> id;
 	public static volatile SingularAttribute<Project, String> name;
 	public static volatile ListAttribute<Project, Employee> employees;
-	public static volatile SingularAttribute<Project, Long> id;
 }
