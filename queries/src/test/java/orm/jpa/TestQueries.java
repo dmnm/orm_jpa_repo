0c8a0468f0b66fb2a7a20a58e7f7d8b421637a6d
dmnm <dm-nm@nm.ru>
2014-05-18 21:14:44 +0400

    11.2

--- queries/src/test/java/orm/jpa/TestQueries.java
+++ queries/src/test/java/orm/jpa/TestQueries.java
@@ -20,32 +20,10 @@ import orm.jpa.entity.Programmer_;
 
 public class TestQueries extends TestCasesJpa {
 
-    public static class EmpDto {
-
-        public Long id;
-        public String name;
-        public String prof;
-        public String department;
-        public String proj;
-
-        public EmpDto(final Long id, final String firstName, final String secondName, final String department, final String proj, final String prof) {
-            this.id = id;
-            this.name = firstName + " " + secondName;
-            this.prof = prof;
-            this.department = department;
-            this.proj = proj;
-        }
-
-        @Override
-        public String toString() {
-            return "EmpDto [id=" + id + ", name=" + name + ", prof=" + prof + ", department=" + department + ", proj=" + proj + "]";
-        }
-    }
-
     @Test
     public void testJpql() {
         final String ql = "select " +
-        		"new orm.jpa.TestQueries.EmpDto(" +
+        		"new orm.jpa.EmpDto(" +
         		" e.id, " +
         		" e.firstName, " +
         		" e.secondName, " +
